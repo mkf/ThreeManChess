199b3506e014499a58a71c28fb351f90074c7aa8
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-04-17 04:13:57 +0200

    placeholder test

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,20 +3,13 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Data.Monoid
 import Control.Monad
-import Utils
+-- import Utils
 -- import Test.QuickCheck
 
-pushTest :: Assertion
-pushTest = [NumLit 1] ^? push (NumLit 1)
-
-pushPopTest :: Assertion
-pushPopTest = [] ^? (push (NumLit 0) >> void pop)
+trueTest :: Assertion
+trueTest = True @=? True
 
 main :: IO ()
 main = defaultMainWithOpts
-       [testCase "push" pushTest
-       ,testCase "push-pop" pushPopTest]
+       [testCase "trueTest" trueTest]
        mempty
-
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
