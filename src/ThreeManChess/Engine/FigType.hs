fc10132c5ee665b13d037aa347b9f572f0b07c50
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-04-17 05:41:43 +0200

    working on figure-parametrized move types and on pawn promotion representation

--- src/ThreeManChess/Engine/FigType.hs
+++ src/ThreeManChess/Engine/FigType.hs
@@ -1,4 +1,4 @@
-{-# Language DeriveDataTypeable #-}
+{-# Language DeriveDataTypeable, DataKinds, KindSignatures #-}
 module ThreeManChess.Engine.FigType where
 
 import Data.Data
@@ -9,3 +9,17 @@ data FigType = InwardPawn | OutwardPawn | Rook | Knight | Bishop | King | Queen 
 -- figTypeSet = Set.fromList [Pawn, Rook, Knight, Bishop, King, Queen]
 figTypes :: [FigType]
 figTypes = [InwardPawn,OutwardPawn,Rook,Knight,Bishop,King,Queen]
+
+data Promotion = RookPromotion | KnightPromotion | BishopPromotion | QueenPromotion
+desiredType :: Promotion -> FigType
+desiredType RookPromotion = Rook
+desiredType KnightPromotion = Knight
+desiredType BishopPromotion = Bishop
+desiredType QueenPromotion = Queen
+
+class PromotionDesire (a :: FigType)
+
+instance PromotionDesire 'Rook
+instance PromotionDesire 'Knight
+instance PromotionDesire 'Bishop
+instance PromotionDesire 'Queen
--- src/ThreeManChess/Engine/Move.hs
+++ src/ThreeManChess/Engine/Move.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE TypeFamilies, DataKinds, ExistentialQuantification, GADTs, KindSignatures #-}
+
 module ThreeManChess.Engine.Move where
 
 import ThreeManChess.Engine.Pos
 import ThreeManChess.Engine.Possibilities
+import ThreeManChess.Engine.FigType
 
 data (Vec a) => BoundVec a = BoundVec a Pos
+
+data KingMove = AloneDiagonally DiagonalDirection | AloneRankwise RankwiseDirection | AloneFilewise FilewiseDirection | NotAlone Castling
+data JustPawnMove = Forward | Capturing FilewiseDirection
+-- data Promotion = forall a . PromotionDesire a => Promotion FigType
+-- data Promotion where
+--   Promotion :: PromotionDesire a => a -> Promotion
+type PossiblyPromotedPawnMove = (JustPawnMove, Maybe Promotion)
+
+type family Move (f :: FigType) where
+  Move 'Queen = LinearVecC
+  Move 'King = KingMove
+  Move 'Rook = StraightVecC
+  Move 'Bishop = LinearVec DiagonalDirection
+  Move 'Knight = KnightVec
+  Move 'InwardPawn = JustPawnMove
+  Move 'OutwardPawn = PossiblyPromotedPawnMove
