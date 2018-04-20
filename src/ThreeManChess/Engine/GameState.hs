973df5d599070ddc68e62a58330f74c2e0406e1b
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-04-20 22:56:00 +0200

    HypoCheckState

--- src/ThreeManChess/Engine/GameState.hs
+++ src/ThreeManChess/Engine/GameState.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module ThreeManChess.Engine.GameState where
 
 import ThreeManChess.Engine.GameBoard
@@ -7,9 +9,15 @@ import ThreeManChess.Engine.Moats
 import ThreeManChess.Engine.EnPassantStore
 import ThreeManChess.Engine.PlayersAlive
 import ThreeManChess.Engine.Directions
+import ThreeManChess.Engine.Pos
 
 data GameState = GameState {board :: GameBoard, moatsState :: MoatsState, movesNext :: Color, castlingPossibilities :: CastlingPossibilities,
                             enPassantStore :: EnPassantStore, halfMoveClock :: Maybe Count, fullMoveCounter :: Maybe Count, playersAlive :: PlayersAlive }
 newGame :: GameState
 newGame = GameState { board = startBoard, moatsState = noBridges, movesNext = White, castlingPossibilities = allCastling,
                       enPassantStore = (Nothing, Nothing), halfMoveClock = Nothing, fullMoveCounter = Nothing, playersAlive = allAlive }
+
+data HypoCheckState = HypoCheckState {board :: GameBoard, enPassantStore :: Maybe Pos, playersAlive :: PlayersAlive}
+
+gameToHypo :: GameState -> HypoCheckState
+gameToHypo GameState{board=b, enPassantStore=e, playersAlive=a} = HypoCheckState{board=b, enPassantStore= lastEnP e, playersAlive=a}
