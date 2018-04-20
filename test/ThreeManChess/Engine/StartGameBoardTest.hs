6997371f49c526a95d0389045077dfa19984a468
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-04-20 10:24:18 +0200

    figTypeAndColorOKTest

--- test/ThreeManChess/Engine/StartGameBoardTest.hs
+++ test/ThreeManChess/Engine/StartGameBoardTest.hs
@@ -9,9 +9,18 @@ import ThreeManChess.Engine.Pos
 import ThreeManChess.Engine.Figure
 import ThreeManChess.Engine.FigType
 import ThreeManChess.Engine.Color
+import ThreeManChess.Engine.Move
+import ThreeManChess.Engine.GameState
 
 whiteKingTest :: Assertion
 whiteKingTest = [(MostOuter, File White kfm)] @=? whereIsFig (Figure King White) startBoard
 
+figTypeAndColorOKTest :: Assertion
+figTypeAndColorOKTest =
+  let sm = StateMove{move=(MkInwardPawnMove (Walk Forward), (SecondOuter, File White (SegmentEight (SegmentQuarter FirstHalf FirstHalf) FirstHalf))),
+                     before=newGame} in
+    True @=? (checkIfFigTypeOK sm && checkIfFigColorOK sm)
+
 tests :: [Test.Framework.Test]
-tests = [testCase "whiteKingTest" whiteKingTest]
+tests = [testCase "whiteKingTest" whiteKingTest,
+         testCase "figTypeAndColorOKTest" figTypeAndColorOKTest]
